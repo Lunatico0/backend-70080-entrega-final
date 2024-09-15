2d8f8f8c208a0372bacb2f07eeecb5570e091c8c
Patricio Pittana <pittanapatricio@gmail.com>
2024-09-14 21:06:46 -0300

    link to vercel

--- src/db.js
+++ src/db.js
@@ -4,6 +4,6 @@ config();
 
 const TOKEN = process.env.URI;
 
-mongoose.connect(TOKEN)
+mongoose.connect(TOKEN, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Error connecting to MongoDB', err));
